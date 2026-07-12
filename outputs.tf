eea1aa7808ac1ee5debd758e495361a08dd7d5a8
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,48 +1,43 @@
-output "api_management_certificates" {
-  description = "All api_management_certificate resources"
-  value       = azurerm_api_management_certificate.api_management_certificates
-  sensitive   = true
-}
 output "api_management_certificates_api_management_name" {
-  description = "List of api_management_name values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.api_management_name }
 }
 output "api_management_certificates_data" {
-  description = "List of data values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.data]
+  description = "Map of data values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.data }
   sensitive   = true
 }
 output "api_management_certificates_expiration" {
-  description = "List of expiration values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.expiration]
+  description = "Map of expiration values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.expiration }
 }
 output "api_management_certificates_key_vault_identity_client_id" {
-  description = "List of key_vault_identity_client_id values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.key_vault_identity_client_id]
+  description = "Map of key_vault_identity_client_id values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.key_vault_identity_client_id }
 }
 output "api_management_certificates_key_vault_secret_id" {
-  description = "List of key_vault_secret_id values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.key_vault_secret_id]
+  description = "Map of key_vault_secret_id values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.key_vault_secret_id }
 }
 output "api_management_certificates_name" {
-  description = "List of name values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.name]
+  description = "Map of name values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.name }
 }
 output "api_management_certificates_password" {
-  description = "List of password values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.password]
+  description = "Map of password values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.password }
   sensitive   = true
 }
 output "api_management_certificates_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.resource_group_name }
 }
 output "api_management_certificates_subject" {
-  description = "List of subject values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.subject]
+  description = "Map of subject values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.subject }
 }
 output "api_management_certificates_thumbprint" {
-  description = "List of thumbprint values across all api_management_certificates"
-  value       = [for k, v in azurerm_api_management_certificate.api_management_certificates : v.thumbprint]
+  description = "Map of thumbprint values across all api_management_certificates, keyed the same as var.api_management_certificates"
+  value       = { for k, v in azurerm_api_management_certificate.api_management_certificates : k => v.thumbprint }
 }
 
